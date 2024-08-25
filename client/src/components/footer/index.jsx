f9837755d60b33a38863cf6ad5ee427017ac922c
Ammar-Amin <ammaramin5253@gmail.com>
2024-08-25 18:58:54 +0530

    install react-redux, redux toolkit. create authSlice, add header & footer

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -1,9 +1,49 @@
+import { WEBSITE_NAME } from '@/constant'
 import React from 'react'
 
 const Footer = () => {
     return (
-        <footer>
-            This will be Our Footer
+        <footer className=" bg-gray-900">
+            <div className="max-w-2xl mx-auto text-white py-10">
+                <div className="text-center">
+                    <h3 className="text-3xl mb-3 px-3"> Download our {WEBSITE_NAME} app </h3>
+                    <p> Stay fit. All day, every day. </p>
+                    <div className="flex flex-col md:flex-row items-center gap-3 justify-center my-10">
+                        <div className="flex items-center border w-48 md:w-auto rounded-lg px-4 py-2 mx-2 cursor-pointer">
+                            <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" className="w-7 md:w-8" />
+                            <div className="text-left ml-3">
+                                <p className='text-xs text-gray-200'>Download on </p>
+                                <p className="text-sm md:text-base"> Google Play Store </p>
+                            </div>
+                        </div>
+                        <div className="flex items-center border w-48 md:w-auto rounded-lg px-4 py-2 mx-2 cursor-pointer">
+                            <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" className="w-7 md:w-8" />
+                            <div className="text-left ml-3">
+                                <p className='text-xs text-gray-200'>Download on </p>
+                                <p className="text-sm md:text-base"> Apple Store </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-28 flex flex-col items-center text-sm text-gray-400">
+                    <ul className="flex items-center justify-center gap-3 md:gap-5 mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            About
+                        </li>
+                        <li>
+                            Privacy Policy
+                        </li>
+                        <li>
+                            Licensing
+                        </li>
+                        <li>
+                            Contact
+                        </li>
+                    </ul>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <span className="block text-sm text-gray-500 text-center dark:text-gray-400">© 2023 <a href='https://github.com/Ammar-Amin' target='_blank'>{WEBSITE_NAME}™</a>. All Rights Reserved.</span>
+            </div>
         </footer>
     )
 }
--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -1,9 +1,96 @@
+import { WEBSITE_NAME } from '@/constant'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { RiMenu3Line } from "react-icons/ri";
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 const Header = () => {
+
+    const authStatus = useSelector(state => state.auth.status)
+    console.log(authStatus)
+
+
+    const navItems = [
+        {
+            title: 'Book Now',
+            route: '/booking',
+            active: !authStatus,
+        },
+        {
+            title: 'Login',
+            route: '/login',
+            active: !authStatus,
+        },
+        {
+            title: 'Book Now',
+            route: '/booking',
+            active: authStatus,
+        },
+        {
+            title: 'Account',
+            route: '/account',
+            active: authStatus,
+        },
+    ]
+
     return (
-        <header>
-            This Will Be our Header
+        <header className='w-full bg-gray-900 text-white'>
+            <div className='max-w-[80rem] mx-auto flex justify-between items-center px-4 py-6'>
+                <Link to='/' className='text-lg md:text-xl hover:text-slate-200'>{WEBSITE_NAME}</Link>
+
+                {/* MOBILE  */}
+                <div className='md:hidden'>
+                    <Sheet>
+                        <SheetTrigger>
+                            <span className='text-xl'>
+                                <RiMenu3Line />
+                            </span>
+                        </SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader>
+                                <SheetTitle></SheetTitle>
+                                <SheetDescription>
+                                    <SheetClose asChild>
+                                        <span className="h-full flex flex-col gap-4 pt-6 ">
+                                            {navItems.map((item) => (item.active &&
+                                                <SheetClose key={item.title} asChild>
+                                                    <Link key={item.title} to={item.route}
+                                                        className='p-2 border text-white font-medium'
+                                                    >
+                                                        {item.title}
+                                                    </Link>
+                                                </SheetClose>
+                                            ))}
+                                        </span>
+                                    </SheetClose>
+                                </SheetDescription>
+                            </SheetHeader>
+                        </SheetContent>
+                    </Sheet>
+                </div>
+
+                {/* TABS & LAPTOPS  */}
+                <div className='hidden md:block'>
+                    <span className="h-full flex gap-4">
+                        {navItems.map((item) => (item.active &&
+                            <Link key={item.title} to={item.route} className='hover:underline'>
+                                {item.title}
+                            </Link>
+                        ))}
+                    </span>
+                </div>
+
+            </div>
         </header>
     )
 }
