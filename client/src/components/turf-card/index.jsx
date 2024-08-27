812a84f04b71cb61bfe2e7905f187f121acf54d0
Ammar-Amin <ammaramin5253@gmail.com>
2024-08-27 13:02:35 +0530

    add single turf page & edit functionality

--- client/src/components/turf-card/index.jsx
+++ client/src/components/turf-card/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const TurfCard = ({ turf }) => {
+    const { isAdmin } = useSelector(state => state.auth.user)
     return (
         <div className="mx-4 mt-4 md:mt-8 w-[300px] rounded-lg border-[1px] border-green-600">
             <img
@@ -17,11 +19,31 @@ const TurfCard = ({ turf }) => {
                 <Link to={`/turf/${turf._id}`}>
                     <button
                         type="button"
-                        className="mt-3 rounded-sm bg-green-600 px-3 py-1 text-[12px] font-semibold text-white shadow-sm hover:bg-green-700"
+                        className="mt-3 rounded-sm bg-green-600 px-3 py-1 text-[12px] font-semibold text-white shadow-sm hover:bg-green-500"
                     >
                         Book Now
                     </button>
                 </Link>
+                {
+                    isAdmin
+                    &&
+                    <>
+                        <Link to={`/update-turf/${turf._id}`}>
+                            <button
+                                type="button"
+                                className="mt-3 ml-[34px] rounded-sm bg-blue-500 px-3 py-1 text-[12px] font-semibold text-white shadow-sm hover:opacity-80"
+                            >
+                                Edit
+                            </button>
+                        </Link>
+                        <button
+                            type="button"
+                            className="mt-3 ml-[34px] rounded-sm bg-red-500 px-3 py-1 text-[12px] font-semibold text-white shadow-sm hover:opacity-80"
+                        >
+                            Delete
+                        </button>
+                    </>
+                }
             </div>
         </div>
     )
--- client/src/pages/SingleTurf.jsx
+++ client/src/pages/SingleTurf.jsx
@@ -1,10 +1,94 @@
-import React from 'react'
+import { BookingForm } from '@/components'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const SingleTurf = () => {
+    const { id } = useParams()
+    const [loading, setLoading] = useState(false)
+    const [turf, setTurf] = useState([])
+    const [openModal, setOpenModal] = useState(false)
+    const [status, setStatus] = useState('Book Now')
+
+    async function getTurf() {
+        setLoading(true)
+        try {
+            const res = await axios.get(
+                `${import.meta.env.VITE_BASE_API}/api/turf/${id}`
+            )
+            // console.log(res)
+            setTurf(res.data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error.response)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getTurf()
+    }, [])
+
+    if (loading) return <div className='min-h-screen flex justify-center items-center'>
+        <h1>Loading...</h1>
+    </div>
+
     return (
-        <div className='min-h-screen flex justify-center items-center'>
-            <span>Single Turf Page</span>
-        </div>
+        <section className='w-full min-h-screen relative'>
+            {
+                turf.length != 0 &&
+                <div className='mt-[76px]'>
+                    <img src={turf.imageUrls[0]}
+                        alt={turf.imageUrls[0]}
+                        className='w-full h-[250px] sm:h-[400px] lg:h-[550px] xl:h-[747px] object-cover'
+                    />
+                    <div className='w-full sm:max-w-xl lg:max-w-2xl xl:max-w-4xl mx-auto p-5 md:py-14 lg:pb-20 flex flex-col gap-4 relative'>
+                        <h1 className='text-2xl md:text-4xl lg:text-5xl'>{turf.name}</h1>
+                        <p className='text-sm md:text-base lg:text-lg text-slate-700'>{turf.desc}</p>
+                        <div className='flex gap-2'>
+                            <div>
+                                <span>Address : </span>
+                                <span>{turf.location.address}, {turf.location.city}, </span>
+                                <br />
+                                <span>{turf.location.state}, {turf.location.pincode}</span>
+                            </div>
+                        </div>
+                        <span>Price : {turf.pricePerHour} / hr</span>
+                        <div className='flex justify-between lg:flex-col lg:gap-2'>
+                            <span>Opening Time : {turf.openingTime}</span>
+                            <span>Closing Time : {turf.closingTime}</span>
+                        </div>
+                        <button
+                            className="mt-3 block mx-auto rounded-sm bg-green-600 px-8 py-2 md:text-lg font-semibold text-white shadow-sm hover:bg-green-700"
+                            disabled={openModal}
+                            onClick={() => setOpenModal(!openModal)}
+                        >
+                            {status}
+                        </button>
+                        {/* LAPTOP FORM  */}
+                        <div className='hidden md:block'>
+                            {
+                                openModal && <BookingForm
+                                    turfId={id}
+                                    setOpenModal={setOpenModal}
+                                    setStatus={setStatus}
+                                />
+                            }
+                        </div>
+                    </div>
+                </div>
+            }
+            {/* MOBILE FORM  */}
+            <div className='md:hidden'>
+                {
+                    openModal && <BookingForm
+                        turfId={id}
+                        setOpenModal={setOpenModal}
+                        setStatus={setStatus}
+                    />
+                }
+            </div>
+        </section>
     )
 }
 
